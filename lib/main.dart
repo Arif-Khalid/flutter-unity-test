2cb9e9284fc2e69165af1a13a10d932de6976ef6
Arif <arifkhalid99@gmail.com>
2023-10-02 12:18:54 +0700

    Initialize repo

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_unity_widget/flutter_unity_widget.dart';
+
+void main() {
+  runApp(const MaterialApp(home: UnityDemoScreen()));
+}
+
+class UnityDemoScreen extends StatefulWidget {
+  const UnityDemoScreen({super.key});
+
+  @override
+  State<UnityDemoScreen> createState() => _UnityDemoScreenState();
+}
+
+class _UnityDemoScreenState extends State<UnityDemoScreen> {
+  static final GlobalKey<ScaffoldState> _scaffoldKey =
+      GlobalKey<ScaffoldState>();
+  late UnityWidgetController _unityWidgetController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      body: SafeArea(
+        bottom: false,
+        child: Container(
+          color: Colors.yellow,
+          child: UnityWidget(
+            onUnityCreated: onUnityCreated,
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Callback that connects the created controller to the unity controller
+  void onUnityCreated(controller) {
+    _unityWidgetController = controller;
+  }
+}
